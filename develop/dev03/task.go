9ed7f6e8f0b7391d832bd07d8473463320d306a9
tensymbols <thetensymbols@gmail.com>
2023-06-06 18:16:02 +0300

    task3

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -38,7 +40,7 @@ func main() {
 	var k int        // column index
 	var n, r, u bool // by numeric value, reverse order, no duplicates
 	var file *os.File
-	flag.IntVar(&k, "k", 0, "column index")
+	flag.IntVar(&k, "k", 1, "column index")
 	flag.BoolVar(&n, "n", false, "sort by numeric value")
 	flag.BoolVar(&r, "r", false, "sort in reverse order")
 	flag.BoolVar(&u, "u", false, "do not print duplicates")
@@ -53,16 +55,58 @@ func main() {
 	} else {
 		log.Fatal("filename is empty")
 	}
-	data, err := io.ReadAll(file)
 
+	data, err := io.ReadAll(file)
 	if err != nil {
 		log.Fatal("could not read file contents")
 	}
+	file.Close()
+
 	var words [][]string
 	for _, v := range strings.Split(string(data), "\n") {
-		words = append(words, strings.Fields(v))
+
+		tokens := strings.Fields(v)
+		if len(tokens) < k {
+			log.Fatal("invalid column index or there is an empty string")
+		}
+		words = append(words, tokens)
 	}
-	sort.Slice(words, func(i, j int) bool {
-		return words[i][k] < words[i][k]
-	})
+
+	if n {
+		sort.Slice(words, func(i, j int) bool {
+			iv, err := strconv.Atoi(words[i][k-1])
+			if err != nil {
+				log.Fatal("non-numeric value was met")
+			}
+			jv, err := strconv.Atoi(words[j][k-1])
+			if err != nil {
+				log.Fatal("non-numeric value was met")
+			}
+			if r {
+				return iv > jv
+			}
+			return iv < jv
+		})
+	} else {
+		sort.Slice(words, func(i, j int) bool {
+			if r {
+				return words[i][k-1] > words[j][k-1]
+			}
+			return words[i][k-1] < words[j][k-1]
+		})
+	}
+	fmt.Println(file, err)
+	file, err = os.OpenFile(flag.Arg(0), os.O_RDWR, 0660)
+	if err != nil {
+		log.Fatal("could not open file for writing")
+	}
+	//	prev:=words[0]
+	for i := range words {
+		_, _ = file.WriteString(strings.Join(words[i], " "))
+		if i < len(words)-1 {
+			file.Write([]byte("\n"))
+		}
+	}
+	fmt.Println(words)
+	file.Close()
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
+	"testing"
+)
+
+type sortTest struct {
+	Input    string
+	Expected string
+}
+
+func TestTask(t *testing.T) {
+	tests := []sortTest{
+		{
+			Input:    "kek b c d\nbek f g h\nz z\nl lll llll l",
+			Expected: "bek f g h\nkek b c d\nl lll llll l\nz z",
+		},
+		{
+			Input:    "a\ne\nc\nh\nb",
+			Expected: "a\nb\nc\ne\nh",
+		},
+	}
+
+	for i, v := range tests {
+		fName := "in" + strconv.Itoa(i) + ".txt"
+		f, err := os.Create(fName)
+		assert.NoError(t, err)
+		_, err = f.WriteString(v.Input)
+		assert.NoError(t, err)
+		err = f.Close()
+		assert.NoError(t, err)
+		exec.Command("go", "run", "./task.go", fName).Run()
+		f, err = os.Open(fName)
+		data, err := io.ReadAll(f)
+
+		assert.NoError(t, err)
+		assert.Equal(t, v.Expected, string(data))
+		err = f.Close()
+		assert.NoError(t, err)
+	}
+}
