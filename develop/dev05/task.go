a06a9f81519545fe8584bbd98f44df22adf34bfe
tensymbols <thetensymbols@gmail.com>
2023-06-08 20:21:48 +0300

    task5

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита grep ===
 
@@ -18,6 +28,98 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func addBefore(lines []string, res []string, currIx int, N int) []string {
+	if N < currIx {
+		return append(res, lines[currIx-N:currIx]...)
+	} else {
+		return append(res, lines[:currIx]...)
+	}
+}
 
+func addAfter(lines []string, res []string, currIx int, N int) []string {
+	if N < len(lines)-currIx-1 {
+		return append(res, lines[currIx+1:currIx+1+N]...)
+	} else {
+		return append(res, lines[currIx+1:]...)
+	}
+}
+
+type grepSettings struct {
+	A, B, C, c int
+	i, v, n, F bool
+}
+
+func main() {
+	gs := grepSettings{}
+
+	flag.IntVar(&gs.A, "A", 0, "after +N lines")
+	flag.IntVar(&gs.B, "B", 0, "before +N lines")
+	flag.IntVar(&gs.C, "C", 0, "context, print A+B lines")
+	flag.IntVar(&gs.c, "c", -1, "count,  lines quantity ")
+
+	flag.BoolVar(&gs.i, "i", false, "ignore case")
+	flag.BoolVar(&gs.v, "v", false, "invert, print everything except matching lines")
+	flag.BoolVar(&gs.n, "n", false, "line num, print line number")
+	flag.BoolVar(&gs.F, "F", false, "explicit match, not a pattern match")
+	flag.Parse()
+	var file *os.File
+	if fName := flag.Arg(0); fName != "" {
+		var err error
+		file, err = os.Open(fName)
+		defer file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal("filename is empty")
+	}
+	pattern := flag.Arg(1)
+	if gs.i {
+		pattern = strings.ToLower(pattern)
+	}
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal("could not read from file")
+	}
+	lines := strings.Split(string(data), "\n")
+	var res []string
+
+	for i := 0; i < len(lines) && gs.c != 0; i++ {
+		curr := ""
+		var match bool
+		if gs.i {
+			match = gs.F && strings.ToLower(lines[i]) == pattern || !gs.F && strings.Contains(strings.ToLower(lines[i]), pattern)
+		} else {
+			match = gs.F && lines[i] == pattern || !gs.F && strings.Contains(lines[i], pattern)
+		}
+		if match && !gs.v {
+			if gs.n {
+				curr += "line " + strconv.Itoa(i) + ":"
+			}
+			if gs.C > 0 {
+				curr += strings.Join(addBefore(lines, []string{}, i, gs.A), "\n")
+				curr += lines[i] + "\n"
+				curr += strings.Join(addAfter(lines, []string{}, i, gs.B), "\n")
+			} else {
+				if gs.A > 0 {
+					curr += strings.Join(addBefore(lines, []string{}, i, gs.A), "\n")
+				}
+				curr += lines[i] + "\n"
+				if gs.B > 0 {
+					curr += strings.Join(addAfter(lines, []string{}, i, gs.B), "\n")
+				}
+			}
+
+		} else if !match && gs.v {
+			if gs.n {
+				curr += "line " + strconv.Itoa(i) + ":"
+			}
+			curr += lines[i] + "\n"
+		}
+		res = append(res, curr)
+		gs.c--
+	}
+	for _, v := range res {
+		fmt.Print(v)
+	}
 }
--- develop/dev05/task_test.go
+++ develop/dev05/task_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os/exec"
+	"testing"
+)
+
+func TestTask(t *testing.T) {
+
+	data, err := exec.Command("go", "run", "./task.go", "-F", "input", "be").CombinedOutput()
+	assert.NoError(t, err)
+	assert.Equal(t, string(data), "be\n")
+
+	data, err = exec.Command("go", "run", "./task.go", "-n", "-v", "input", "be").CombinedOutput()
+	assert.NoError(t, err)
+	fmt.Println(string(data))
+	assert.Equal(t, string(data), "line 1:ek elsdf sdf\r\nline 2:fdsdfsfds dsfsdfsf\r\n")
+}
